a81f0f9c94ce7e8c387b50073fe46f7a727fa6c6
wangy <871406098@qq.com>
2019-11-28 10:30:14 +0800

    8888

---
+++ src/main/java/com/example/jenkinstest/controller/MyCon.java
@@ -0,0 +1,20 @@
+package com.example.jenkinstest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: com.example.jenkinstest.controller.MyCon
+ * @description:
+ * @author: Mr.BULLET
+ * @create: 2019-11-28 10:29
+ */
+@RestController
+public class MyCon {
+    @RequestMapping("/a")
+    public String aaa(){
+        return "好了";
+    }
+
+}
